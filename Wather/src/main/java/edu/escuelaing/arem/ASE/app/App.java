f8caf6da25bd4c342cf0e703e848d170c3548db8
lale1507 <laurisaleja15.15@gmail.com>
2021-09-08 15:27:34 -0500

    implemetación Weather

--- Wather/src/main/java/edu/escuelaing/arem/ASE/app/App.java
+++ Wather/src/main/java/edu/escuelaing/arem/ASE/app/App.java
@@ -1,13 +1,30 @@
 package edu.escuelaing.arem.ASE.app;
 
+import com.google.gson.Gson;
+import edu.escuelaing.arem.ASE.app.weather.WeatherH;
+
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+
+import static spark.Spark.*;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        port(getPort());
+        Gson gson = new Gson();
+
+        get("/clima", WeatherH.handle);
+
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set
     }
 }
--- Wather/src/main/java/edu/escuelaing/arem/ASE/app/weather/WeatherH.java
+++ Wather/src/main/java/edu/escuelaing/arem/ASE/app/weather/WeatherH.java
@@ -1,2 +1,72 @@
-package edu.escuelaing.arem.ASE.app.weather;public class WeatherH {
+package edu.escuelaing.arem.ASE.app.weather;
+
+//import edu.eci.arep.cache.WeatherCache;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+public class WeatherH{
+    public static Route handle = (Request req, Response res) -> {
+        String baseURL = "https://api.openweathermap.org/data/2.5/weather";
+        String appid = "14448a44c8d0ca3f6244009a1b9eee49";
+        HashMap<String, String> paramMap = new HashMap<>();
+
+        String place = req.queryParams("lugar");
+        if (place != null) {
+            res.status(200);
+            res.type("application/json");
+
+            paramMap.put("q", place);
+            paramMap.put("appid", appid);
+            String URL = createURL(baseURL, paramMap);
+            return getData(place, URL);
+        } else {
+            res.status(400);
+            return "Error: No se especificó un lugar";
+        }
+    };
+
+    private static String createURL(String baseURL, HashMap<String, String> params) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(baseURL).append("?");
+        for (Map.Entry<String, String> e : params.entrySet()) {
+            sb.append(e.getKey()).append("=").append(e.getValue()).append("&");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
+    }
+
+    private static String getData(String place, String URL) throws IOException {
+        String cachedPlace = WeatherCache.searchPlace(place);
+        String data;
+        if (cachedPlace == null) {
+            System.out.println("NOT CACHED");
+            data = readURL(URL);
+            WeatherCache.putPlace(place, data);
+            return data;
+        } else {
+            System.out.println("CACHED");
+            return cachedPlace;
+        }
+    }
+
+    private static String readURL(String URL) throws IOException {
+        InputStream is = new URL(URL).openStream();
+        BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
 }
